579e51e2008888154c8ebb9c6b42be825021e937
AlexDavis <a.larsendavis@outlook.com>
2022-09-08 22:21:08 -0500

    CommuneApp, CREATE task calendar

---
+++ CommuneApp/server/models/User.js
@@ -0,0 +1,13 @@
+//Use Modules here
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+//this is a mongoose constructor
+let userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+})
+
+userSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
